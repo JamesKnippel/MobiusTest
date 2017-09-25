3db805ee3239d9e9615b21a5192176f8ea33be3d
James Knippel <jamesknippel@gmail.com>
2017-09-25 12:37:15 -0700

    prepared event listener for feed

--- rubyPal/src/app/transactions/transactions.component.ts
+++ rubyPal/src/app/transactions/transactions.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-
+import {Component, OnInit, OnDestroy} from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { Subject } from 'rxjs/Subject';
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
   styleUrls: ['./transactions.component.sass']
 })
-export class TransactionsComponent implements OnInit {
+export class TransactionsComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngOnDestroy() {
+    
+  }
 }
--- rubyPal/src/app/user/user.component.ts
+++ rubyPal/src/app/user/user.component.ts
@@ -3,6 +3,13 @@ import { User } from '../models/users/user.interface';
 import { PaymentService } from '../services/payment.service';
 import { Angular2TokenService } from 'angular2-token';
 
+/* we'll be sending this data to our feed */
+export interface OnSendTransactionEvent {
+  sender: string;
+  receiver: string;
+  value: number;
+}
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -15,12 +22,22 @@ export class UserComponent implements OnInit {
 
   @Input() user: User;
 
+  @Output()
+  public onSendTransaction: EventEmitter<OnSendTransactionEvent> = new EventEmitter<OnSendTransactionEvent>();
+
   constructor(private paymentService: PaymentService,
               private authTokenService: Angular2TokenService) { }
 
   async sendTransaction(): Promise<any> {
+    /* calls our payment service to update the database */
     await this.paymentService.addCredits(this.user.email, this.amount);
     this.paymentService.subtractCredits(this.authTokenService.currentUserData.email, this.amount);
+    /* event to tell our feed there's a need to add a row */
+    this.onSendTransaction.emit({
+      sender: this.authTokenService.currentUserData.email,
+      receiver: this.user.email,
+      value: this.amount
+    });
   }
 
   ngOnInit() {
